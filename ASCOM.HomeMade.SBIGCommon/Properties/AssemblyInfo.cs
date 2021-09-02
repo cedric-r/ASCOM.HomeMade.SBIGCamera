a9625a418072cd0eb18b357de52a306135b51caf
Cedric Raguenaud <32923629+cedric-r@users.noreply.github.com>
2021-09-02 10:27:45 +0200

    Version 1.0.0.

--- ASCOM.HomeMade.SBIGCommon/Properties/AssemblyInfo.cs
+++ ASCOM.HomeMade.SBIGCommon/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 //
 // TODO - Set your driver's version here
-[assembly: AssemblyVersion("0.22.39.0")]
-[assembly: AssemblyFileVersion("0.22.39.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
